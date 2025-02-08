63543ab1848d4be4fff513fe8e9eb70e99f35856
Hemalatha M <hemalatham236@gmail.com>
2025-02-08 14:39:19 +0530

    app save

--- hemalatha/app.py
+++ hemalatha/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import pickle
 
 app = Flask(__name__)
 
@@ -13,3 +14,38 @@ def ping():
 @app.route("/jjjjjj", methods = ["GET"])
 def jjjjjj():
     return "<p>Hey man! i'm hear"
+
+model_pickle = open(r"C:\Users\DELL\Desktop\MLOPs\Flask\hemalatha\classifier.pkl", "rb")
+clf = pickle.load(model_pickle)
+
+# defining the endpoint which will make the prediction
+@app.route("/prediction", methods=['POST'])
+def prediction():
+    """ Returns loan application status using ML model
+    """
+    loan_req = request.get_json()
+    print(loan_req)
+    if loan_req['Gender'] == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+    if loan_req['Married'] == "Unmarried":
+        Married = 0
+    else:
+        Married = 1
+    if loan_req['Credit_History'] == "Unclear Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1
+
+    ApplicantIncome = loan_req['ApplicantIncome']
+    LoanAmount = loan_req['LoanAmount']
+
+    result = clf.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+
+    if result == 0:
+        pred = "Rejected"
+    else:
+        pred = "Approved"
+
+    return {"loan_approval_status": pred}
